7baa9838d21cccc8ac3180f665991bd934c97991
Navyashree008 <75984629+Navyashree008@users.noreply.github.com>
2021-02-15 10:22:03 +0530

    Add files via upload

---
+++ len.py
@@ -0,0 +1,13 @@
+numbers=[50, 40, 23, 70, 56, 12, 5, 10, 7] 
+a = []
+i = 0
+length = 0
+while i >= 0:
+    a.append(numbers[i])
+    length = length+1
+    if numbers == a:
+        print(length)
+        break
+    i+=1
+
+    
